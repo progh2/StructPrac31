102317731585ce9c2273316ee96eb0ad6ea3f64a
Gihun Ham <ham@e-mirim.hs.kr>
2023-09-04 14:34:36 +0900

    초기 커밋.

---
+++ StructPrac31/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructPrac31
+{
+    internal class Program
+    {
+        struct Point
+        {
+            public int x;
+            public int y;
+        }
+
+
+        static void Main(string[] args)
+        {
+            Point p;
+            p.x = 10;
+            p.y = 10;
+            Console.WriteLine($"{p.x} - {p.y}");
+        }
+    }
+}
